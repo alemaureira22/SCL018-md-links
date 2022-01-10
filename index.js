e7c6d5c2b8965ea71bec0fbfdd06dc10afe5a711
alemaureira22 <jandrita_18@hotmail.com>
2022-01-10 19:01:37 -0300

    creacion readme ok

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 import fetch from "node-fetch";
 import fs from "fs";
 import path from "path";
@@ -9,46 +8,45 @@ const path = require('path');
 const fetch = require('node-fetch');
 const Yargs = require('yargs');*/
 
-
 const userPath = process.argv[2];
 const option = Yargs(process.argv.slice(2)).argv;
-const regEx = /\[([^\]]+)]\((https?:\/\/[^\s)]+)\)/g;//expresion regular
+const regEx = /\[([^\]]+)]\((https?:\/\/[^\s)]+)\)/g; //expresion regular
 
 const mdFile = (fileToRead) => {
-const ext = path.extname(fileToRead.toLowerCase());
+  const ext = path.extname(fileToRead.toLowerCase());
   if (ext === ".md") {
     return true;
   } else {
     console.log("No es un archivo md");
   }
-
 };
 
 const readUserFile = (readFile) => {
   try {
-    const data = fs.readFileSync(readFile,"utf8");
+    const data = fs.readFileSync(readFile, "utf8");
     return searchLinks(data);
-    }catch (err) {
+  } catch (err) {
     console.log(err);
   }
 };
 // funcion para leer los links
 const searchLinks = (path) => {
-  const lines = path.split("\n");//separa en lineas el documento
+  const lines = path.split("\n"); //separa en lineas el documento
   let allLinks = [];
   for (let i = 0; i < lines.length; i++) {
     const line = lines[i];
-    const links = line.matchAll(regEx);// devuelve arreglo - leer los link
+    const links = line.matchAll(regEx); // devuelve arreglo - leer los link
     const match = regEx.test(line);
     if (match) {
-      for (const link of links) { // devuelve un iterable
+      for (const link of links) {
+        // devuelve la informacion
         const data = {
           text: link[1],
           href: link[2],
           path: userPath,
           line: i + 1,
         };
-        allLinks.push(data);
+        allLinks.push(data); // lo agregamos al array vacio y retorna
       }
     }
   }
@@ -56,26 +54,28 @@ const searchLinks = (path) => {
 };
 // validar links
 const urlValidate = (links) => {
-  const validated = links.map((link) =>//calback
-    fetch(link.href).then((response) => {
-       return {
-        text: link.text,
-        href: link.href,
-        path: link.path,
-        line: link.line,
-        status: response.status,
-        statusText: response.statusText,
-      
-      };
-    })
+  const validated = links.map(
+    (
+      link //nos devuelve un array/ for e
+    ) =>
+      fetch(link.href).then((response) => {
+        return {
+          text: link.text,
+          href: link.href,
+          path: link.path,
+          line: link.line,
+          status: response.status,
+          statusText: response.statusText,
+        };
+      })
   );
-  return Promise.all(validated);
+  return Promise.all(validated); // tiene que aprobar todas las promesas
 };
 
-// funcio padre
-const mdLinks = (readFile) => {
-   return new Promise((resolve, reject) => {
-    const links = readUserFile(readFile);
+// funcio madre
+const mdLinks = (readFile,option) => {
+  return new Promise((resolve, reject) => {
+    const links = readUserFile(readFile); // contiene objeto
     if (option.validate) {
       resolve(urlValidate(links));
     } else {
@@ -85,12 +85,6 @@ const mdLinks = (readFile) => {
   });
 };
 
-mdLinks(userPath).then((results) => console.table(results));
+mdLinks(userPath,option).then((results) => console.table(results));
 
-export { 
-  mdLinks,
-  searchLinks,
-  readUserFile,
-  urlValidate, 
-  mdFile 
-};
+export { mdLinks, searchLinks, readUserFile, urlValidate, mdFile };
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,21 +1,21 @@
- import {
+import {
   mdLinks,
   mdFile,
   readUserFile,
   searchLinks,
-  urlValidate
-
+  urlValidate,
 } from "../index.js";
- const path = 'test/test1.md';
- 
- describe("mdFile", () => {
-  it('debería ser una true', () => {
-    const result =mdFile(path)
+const path = "test/test1.md";
+const option = "--validate";
+
+describe("mdFile", () => {
+  it("debería ser una true", () => {
+    const result = mdFile(path);
     expect(result).toBeTruthy();
   });
   it("debería retornar false", () => {
-    const result = mdFile('chao');
-    expect(result).toBeFalsy()
+    const result = mdFile("chao");
+    expect(result).toBeFalsy();
   });
 });
 
@@ -23,58 +23,49 @@ describe("readUserFile", () => {
   it("deberia retornar un arreglo de objetos", () => {
     const result = [
       {
-        text: 'Sitio oficial de GitHub Pages',
-        href: 'https://pages.github.com/',
-        path: '--coverage',
-        line: 1
+        text: "Sitio oficial de GitHub Pages",
+        href: "https://pages.github.com/",
+        path: "--coverage",
+        line: 1,
       },
       {
-        text: 'Pixar',
-        href: 'https://www.pixar.com/error404',
-        path: '--coverage',
-        line: 3
-      }];
+        text: "Pixar",
+        href: "https://www.pixar.com/error404",
+        path: "--coverage",
+        line: 3,
+      },
+    ];
     expect(readUserFile(path)).toEqual(result);
   });
- 
-
 });
 describe("searchLinks", () => {
-  it('debería retornar un array', () => {
+  it("debería retornar un array", () => {
     const result = searchLinks(path);
     expect(result).toBeInstanceOf(Array);
   });
   it("debería retornar un objeto", () => {
-    const result =  searchLinks(path);
+    const result = searchLinks(path);
     expect(result).toBeInstanceOf(Array);
   });
 });
- describe("urlValidate", () => {
-   it('debería mockear', () => {
-    expect(typeof urlValidate).toBe('function');
+describe("urlValidate", () => {
+  it("debería mockear", () => {
+    expect(typeof urlValidate).toBe("function");
   });
 });
-   // it("debería retornar un Array", () => {
-   // const result = urlValidate(path);c
-   // expect(result).toBeInstanceOf('function');
-    
- // });
-  describe("mdLinks", () => {
-    it("deberia retornar una promesa", () => {
-      const result = mdLinks(path);
-      expect(result).toBeInstanceOf(Promise);
+// it("debería retornar un Array", () => {
+// const result = urlValidate(path);c
+// expect(result).toBeInstanceOf('function');
+
+// });
+describe("mdLinks", () => {
+  //it("deberia retornar una promesa", () => {
+  // const result = mdLinks(path);
+  // expect(result).toBeInstanceOf(Promise);
+  //});
+  it("deberia retornar una promesa", () => {
+    return mdLinks(path, option).then((res) => {
+      expect(res).toBeInstanceOf(Array);
     });
-    it("deberia retornar una promesa", () => {
-      expect.assertions();
-      return mdLinks(path)
-    
-      .then(res => {
-        console.log(res);
-       expect(res).toBeInstanceOf(Array);
-  
-      });
-  
   });
-  
-  });
-  
+});
